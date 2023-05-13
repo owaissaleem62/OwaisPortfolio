003a48c68aedb8537fc0a7aedd64bbbecaad74c0
Owais Saleem <saleemowais62@gmail.com>
2023-05-13 16:50:20 +0500

    project add

--- script.js
+++ script.js
@@ -35,14 +35,15 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["FrontEnd Developer"],
+        strings: ["Front-End Developer","Wordpress Developer "]
+        ,
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["FrontEnd Developer"],
+        strings: ["Front-End Developer" ,"Wordpress Developer "],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
